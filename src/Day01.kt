52c8b2f0432a65b8ab08ed522d7cc7bf3cc8984c
Greg Joseph <greg.jos@gmail.com>
2023-12-05 20:34:57 +1100

    Day 01 part 2 - brute force with a regex, how elegant

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,37 @@
+import java.util.regex.MatchResult
+import java.util.regex.Pattern
+
 fun main() {
+    fun fromDigits(first: Int, last: Int, debug: String): Int {
+        // woah maths
+        val i: Int = (first * 10) + last
+        // println("* ${debug} --> ${i}")
+        return i
+    }
+
     fun part1(input: List<String>): Int {
         return input.map { line: String ->
             val digits = line.toCharArray().filter(Character::isDigit)
-            // lol
-            val i: Int = (digits.first().digitToInt() * 10) + digits.last().digitToInt()
-            // println("line = ${line} --> ${i}")
-            i
+            fromDigits(digits.first().digitToInt(), digits.last().digitToInt(), line)
         }.sum()
     }
 
+
+    val NUMBERS_PATTERN = Pattern.compile("(\\d|one|two|three|four|five|six|seven|eight|nine)")
+    val NUMBERS = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+    fun stringToInt(it: String) = if (NUMBERS.contains(it)) NUMBERS.indexOf(it) + 1 else it.toInt()
+
+    fun forLine(s: String): Int {
+        // how elegant
+        val nums = NUMBERS_PATTERN.matcher(s).results()
+            .map(MatchResult::group)
+            .map(::stringToInt).toList()
+        return fromDigits(nums.first(), nums.last(), s)
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map(::forLine).sum()
     }
 
     // test if implementation meets criteria from the description, like:
