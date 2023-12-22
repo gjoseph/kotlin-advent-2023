412b4fcf70b64176679b6a216c6a4bd089a22b87
Greg Joseph <greg.jos@gmail.com>
2023-12-22 21:39:26 +1100

    Day 08 - part 2 - this was fucking bullshit. Random luck/intuition was correct, except my lcm() function overflew silently

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -1,14 +1,27 @@
 package day08
 
 import kotlin.time.measureTimedValue
+import lcm
 import printResult
 import readDayInput
 import readTestInput
 import requireSize
 
 enum class Direction { L, R }
-data class Destinations(val left:String, val right:String)
-data class Doc(val directions: List<Direction>,val map: Map<String, Destinations>)
+data class Destinations(val left: String, val right: String)
+data class Doc(val directions: List<Direction>, val map: Map<String, Destinations>) {
+
+    fun nextStep(
+        currentMoveCount: Int,
+        currentLocation: String,
+    ): String {
+        val d = this.directions[currentMoveCount % this.directions.size]
+        return when (d) {
+            Direction.L -> map.get(currentLocation)!!.left
+            Direction.R -> map.get(currentLocation)!!.right
+        }
+    }
+}
 
 fun main() {
     fun parse(input: List<String>): Doc {
@@ -21,47 +34,51 @@ fun main() {
     }
 
     fun part1(input: List<String>): Int {
-        val (directions, map) = parse(input)
+        val doc = parse(input)
         var moves = 0
         var curr = "AAA"
         while (curr != "ZZZ") {
-            val d = directions[moves % directions.size]
-            curr = when (d) {
-                Direction.L -> map.get(curr)!!.left
-                Direction.R -> map.get(curr)!!.right
-            }
+            curr = doc.nextStep(moves, curr)
             moves++
         }
         return moves
     }
 
-    fun part2(input: List<String>): Int {
-        val (directions, map) = parse(input)
-        var moves = 0
-        var curr = map.filterKeys { it.endsWith('A') }.keys
-        var bestZCount = 0
-        while (!curr.all { it.endsWith('Z') }) {
-            val d = directions[moves % directions.size]
-            curr = curr.map {
-                when (d) {
-                    Direction.L -> map.get(it)!!.left
-                    Direction.R -> map.get(it)!!.right
-                }
-            }.toSet()
-            val zCount = curr.count { it.endsWith('Z') }
-            if (zCount>bestZCount) {
-                bestZCount = zCount
-                println("move #${moves} -- $curr -> $bestZCount")
+
+    fun part2(input: List<String>): Long {
+        val doc = parse(input)
+        // solve each route individually, and hope the least common multiple is the right answer
+        // also hope that each starting point leads to a _different_ endpoint ... (it does)
+        // and... fuck me, why did my lcm() not blow up when it overflew
+        var startingPoints = doc.map.filterKeys { it.endsWith('A') }.keys
+
+        // Empirically, we've verified that all routes loop in on themselves once we reach the end
+        data class Route(val initialMovesToDest: Int, val loopLength: Int)
+
+        val results: List<Route> = startingPoints.map { start ->
+            var moves = 0
+            var curr = start
+            val route = mutableListOf(curr)
+            while (!curr.endsWith('Z')) {
+                curr = doc.nextStep(moves, curr)
+                moves++
+                route.add(curr)
             }
-            moves++
+            val next = doc.nextStep(moves, curr)
+            check(route.contains(next)) { "Empirically, we've verified all routes are looping on themselves" }
+            val startOfLoop = route.indexOf(next)
+            val loopMoves = route.size - startOfLoop
+            // ... but that turned out to be absolutely useless
+            Route(moves, loopMoves)
         }
-        return moves
+        // because in the end, all we needed was for lcm() to not overflow silently (i.e use Long instead of Int) and that was the answer... :facepalm:
+        return lcm(results.map { it.initialMovesToDest.toLong() })
     }
 
     // tests
     check(part1(readTestInput(1)) == 2)
     check(part1(readTestInput(2)) == 6)
-    check(part2(readTestInput(3)) == 6)
+    check(part2(readTestInput(3)) == 6L)
 
     val input = readDayInput()
     printResult(1, measureTimedValue { part1(input) })
